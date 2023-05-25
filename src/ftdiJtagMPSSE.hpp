46fd6e12f2446e3fd992f6cd63cfeb5c89aee612
wonderfullook <123hyx456@gmail.com>
2023-05-25 22:10:04 +0800

    upadte sipeed tang chip model

--- src/ftdiJtagMPSSE.hpp
+++ src/ftdiJtagMPSSE.hpp
@@ -80,7 +80,7 @@ class FtdiJtagMPSSE : public JtagInterface, public FTDIpp_MPSSE {
 	 *        pos is used for write and neg to sample
 	 */
 	void config_edge();
-	bool _ch552WA; /* avoid errors with SiPeed tangNano */
+	bool _ch552WA; /* avoid errors with Sipeed tangNano */
 	uint8_t _write_mode; /**< write edge configuration */
 	uint8_t _read_mode; /**< read edge configuration */
 	bool _invert_read_edge; /**< read edge selection (false: pos, true: neg) */
